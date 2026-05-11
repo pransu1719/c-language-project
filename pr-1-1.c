3f2733e35f2c4bf6c86d059316b9de844402da15
pransu1719 <pransupatel452@gmail.com>
2026-05-11 17:42:46 +0530

    Add files via upload

---
+++ pr-1-1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	float c;
+	printf("enter tempreture in celsius:",c);
+	scanf("%f",&c);
+	float f=(9/5*c +32);
+	printf("enter tempreture in fahrenhit:%.2f",f);
+	
+	
+}
